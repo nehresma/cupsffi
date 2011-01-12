9f2c75487e74b0dd913ffc620c272cdd515aa9eb
Nathan Ehresman <nehresma@gmail.com>
2011-01-11 21:11:44 -0500

    Free up dests

--- lib/cupsffi/cups.rb
+++ lib/cupsffi/cups.rb
@@ -1,20 +1,21 @@
 class Cups
   def self.get_printer_names
     p = FFI::MemoryPointer.new :pointer
-    s = CupsFFI::cupsGetDests(p)
+    dest_count = CupsFFI::cupsGetDests(p)
     ary = []
-    s.times do |i|
+    dest_count.times do |i|
       d = CupsFFI::CupsDestS.new(p.get_pointer(i))
       ary.push(d[:name])
     end
+    CupsFFI::cupsFreeDests(dest_count, p)
     ary
   end
 
   def self.get_printer_options(printer_name)
     p = FFI::MemoryPointer.new :pointer
-    dests = CupsFFI::cupsGetDests(p)
+    dest_count = CupsFFI::cupsGetDests(p)
     ary = []
-    dests.times do |i|
+    dest_count.times do |i|
       dest = CupsFFI::CupsDestS.new(p.get_pointer(i))
       next unless dest[:name] == printer_name
       dest[:num_options].times do |j|
@@ -22,6 +23,7 @@ class Cups
         ary.push({:name => options[:name], :value => options[:value]})
       end
     end
+    CupsFFI::cupsFreeDests(dest_count, p)
     ary
   end
 end
